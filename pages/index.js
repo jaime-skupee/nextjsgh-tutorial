95b07cf3a04d9f8331190926ef7447b47f22051e
jaime-skupee <rodriguez@skupee.online>
2023-03-17 13:52:30 -0600

    updated workflow

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Nextjs CICD
+          Nextjs CICD Hello World PM2
         </h1>
 
         <p className={styles.description}>
